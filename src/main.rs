ca04e9ee93ebdf91a0ce62e5f36be717c0c2dfdf
Shanah Jr Suping <shanahjr@gmail.com>
2023-03-21 19:23:48 +0200

    Using env to hopefully get port from Railway variable

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    env
 };
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:2121").unwrap();
+
+    let port = env::var("PORT")
+        .expect("Variable for port not found");
+
+    let listener = TcpListener::bind(format!("{}{}","0.0.0.0:",port))
+        .expect("Failed to bind to port");
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream = stream.expect("Failed to create TCP stream");
 
         handle_connection(stream);
     }
@@ -18,16 +24,18 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let _http_request: Vec<_> = buf_reader
         .lines()
-        .map(|result| result.unwrap())
+        .map(|result| result
+                .expect("faild to get result rom http request lines"))
         .take_while(|line| !line.is_empty())
         .collect();
 
     let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("./html/hello.html").unwrap();
+    let contents = fs::read_to_string("./html/hello.html")
+        .expect("Error finding html file");
     let length = contents.len();
 
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).expect("Failed to write response");
 }
